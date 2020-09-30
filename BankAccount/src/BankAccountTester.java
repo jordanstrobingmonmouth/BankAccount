db21c1af63bb20c79f2242974fe659226a2d1823
jordanstrobing <jordanstrobing@localhost>
2020-09-30 14:08:37 -0400

    First commit

---
+++ BankAccount/src/BankAccountTester.java
@@ -0,0 +1,12 @@
+
+public class BankAccountTester {
+
+	public static void main(String[] args) {
+		BankAccount myBankAccount = new BankAccount(1000);
+		myBankAccount.withdraw(500);
+		myBankAccount.deposit(1500);
+		System.out.println("Your balance is: " + myBankAccount.getBalance());
+
+	}
+
+}
